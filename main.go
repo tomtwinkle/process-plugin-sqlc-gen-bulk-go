0055d49e792ebde0d72bb05233875349dcc81970
tom twinkle <tomtinkle29@gmail.com>
2025-06-20 17:35:53 +0900

    Supports Insert Query with Extra Suffix
    Add CI test
    
    Add CODEOWNERS
    
    fix test

--- main.go
+++ main.go
@@ -8,7 +8,13 @@ import (
 	"github.com/sqlc-dev/plugin-sdk-go/plugin"
 )
 
-const generateFileName = "bulk.sql.go"
+const (
+	generateFileName = "bulk.sql.go"
+
+	sourceTemplateFuncPath = "templates/template.go"
+	sourceTemplateFunc1    = "extractFieldValues"
+	sourceTemplateFunc2    = "buildBulkInsertQuery"
+)
 
 func main() {
 	codegen.Run(Generate)
@@ -37,14 +43,31 @@ func Generate(ctx context.Context, req *plugin.GenerateRequest) (*plugin.Generat
 func generate(
 	ctx context.Context, req *plugin.GenerateRequest, opts *Options, structs BulkInserts,
 ) (*plugin.GenerateResponse, error) {
+	extractFieldValuesFn, err := parseGoCode(sourceTemplateFuncPath, sourceTemplateFunc1)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse function %s: %w", sourceTemplateFunc1, err)
+	}
+	buildBulkInsertQueryFn, err := parseGoCode(sourceTemplateFuncPath, sourceTemplateFunc2)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse function %s: %w", sourceTemplateFunc2, err)
+	}
+
 	tmpl := struct {
-		Package     string
-		SqlcVersion string
-		BulkInsert  []BulkInsert
+		Package       string
+		SqlcVersion   string
+		BulkInsert    []BulkInsert
+		ExtractFnName string
+		ExtractFn     string
+		BuildFnName   string
+		BuildFn       string
 	}{
-		Package:     opts.Package,
-		SqlcVersion: req.GetSqlcVersion(),
-		BulkInsert:  structs,
+		Package:       opts.Package,
+		SqlcVersion:   req.GetSqlcVersion(),
+		BulkInsert:    structs,
+		ExtractFnName: sourceTemplateFunc1,
+		ExtractFn:     string(extractFieldValuesFn),
+		BuildFnName:   sourceTemplateFunc2,
+		BuildFn:       string(buildBulkInsertQueryFn),
 	}
 
 	code, err := executeTemplate(ctx, "bulkInsertFile", tmpl)
